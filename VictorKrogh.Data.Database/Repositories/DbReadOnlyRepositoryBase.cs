4f4976291454eddd9414c1a98b9aa2379c45ac31
KingoBoiii <kingodk@outlook.dk>
2024-03-19 18:10:19 +0100

    Added base implementation for database repository

---
+++ VictorKrogh.Data.Database/Repositories/DbReadOnlyRepositoryBase.cs
@@ -0,0 +1,73 @@
+﻿using System.Data;
+using VictorKrogh.Data.Database.Models;
+using VictorKrogh.Data.Database.Providers;
+using VictorKrogh.Data.Repositories;
+
+namespace VictorKrogh.Data.Database.Repositories;
+
+public abstract class DbReadOnlyRepositoryBase<TModel> : DbRepositoryBase, IReadOnlyRepository<TModel>
+    where TModel : Model
+{
+    protected DbReadOnlyRepositoryBase(IDbProvider provider)
+        : base(provider)
+    {
+    }
+
+    protected async virtual ValueTask<TModel?> QuerySingleAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.QuerySingleAsync<TModel>(sql, parameters, commandTimeout, commandType);
+    }
+
+    protected async virtual ValueTask<TModel?> QuerySingleOrDefaultAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.QuerySingleOrDefaultAsync<TModel>(sql, parameters, commandTimeout, commandType);
+    }
+
+    protected async virtual ValueTask<TModel?> QueryFirstAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.QueryFirstAsync<TModel>(sql, parameters, commandTimeout, commandType);
+    }
+
+    protected async virtual ValueTask<TModel?> QueryFirstOrDefaultAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.QueryFirstOrDefaultAsync<TModel>(sql, parameters, commandTimeout, commandType);
+    }
+
+    protected async virtual ValueTask<IEnumerable<TModel?>> QueryAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.QueryAsync<TModel>(sql, parameters, commandTimeout, commandType);
+    }
+
+    public abstract ValueTask<IEnumerable<TModel?>> GetAsync();
+
+    public async virtual ValueTask<TModel?> GetFirstOrDefaultAsync()
+    {
+        var result = await GetAsync();
+        if (result == default)
+        {
+            return default;
+        }
+
+        return result.FirstOrDefault();
+    }
+}
+
+public abstract class DbReadOnlyRepositoryBase<TModel, TKey> : DbReadOnlyRepositoryBase<TModel>, IReadOnlyRepository<TModel, TKey>
+    where TModel : Model
+    where TKey : notnull
+{
+    protected DbReadOnlyRepositoryBase(IDbProvider provider)
+        : base(provider)
+    {
+    }
+
+    public async ValueTask<TModel?> GetAsync(TKey key)
+    {
+        return await Provider.GetAsync<TModel, TKey>(key);
+    }
+
+    public override async ValueTask<IEnumerable<TModel?>> GetAsync()
+    {
+        return await Provider.GetAllAsync<TModel>();
+    }
+}
---
+++ VictorKrogh.Data.Database/Repositories/DbRepositoryBase.cs
@@ -0,0 +1,74 @@
+﻿using System.Data;
+using VictorKrogh.Data.Database.Models;
+using VictorKrogh.Data.Database.Providers;
+using VictorKrogh.Data.Repositories;
+
+namespace VictorKrogh.Data.Database.Repositories;
+
+public abstract class DbRepositoryBase : RepositoryBase<IDbProvider>
+{
+    protected DbRepositoryBase(IDbProvider provider) 
+        : base(provider)
+    {
+    }
+
+    protected async ValueTask<int> ExecuteAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.ExecuteAsync(sql, parameters, commandTimeout, commandType);
+    }
+
+    protected async ValueTask<T?> ExecuteScalarAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.ExecuteScalarAsync<T>(sql, parameters, commandTimeout, commandType);
+    }
+
+    protected async ValueTask<T?> ExecuteSingleOrDefaultAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.ExecuteSingleOrDefaultAsync<T>(sql, parameters, commandTimeout, commandType);
+    }
+
+    protected async ValueTask<IEnumerable<T?>> ExecuteQueryAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    {
+        return await Provider.ExecuteQueryAsync<T>(sql, parameters, commandTimeout, commandType);
+    }
+}
+
+public abstract class DbRepositoryBase<TModel, TKey> : DbReadOnlyRepositoryBase<TModel, TKey>, IRepository<TModel, TKey>
+    where TModel : Model
+    where TKey : notnull
+{
+    protected DbRepositoryBase(IDbProvider provider)
+        : base(provider)
+    {
+    }
+
+    public async virtual ValueTask<bool> AddAsync(TModel model)
+    {
+        return await Provider.InsertAsync(model);
+    }
+
+    public async virtual ValueTask<bool> UpdateAsync(TModel model)
+    {
+        return await Provider.UpdateAsync(model);
+    }
+
+    public async virtual ValueTask<bool> AddOrUpdateAsync(TModel model)
+    {
+        if (model.IsTransient())
+        {
+            return await AddAsync(model);
+        }
+
+        return await UpdateAsync(model);
+    }
+
+    public async virtual ValueTask<bool> DeleteAsync(TModel model)
+    {
+        return await Provider.DeleteAsync(model);
+    }
+
+    public async virtual ValueTask<bool> DeleteAllAsync()
+    {
+        return await Provider.DeleteAllAsync<TModel>();
+    }
+}
